369c23b789fccc92ac297c6a8f1587d1df8ff716
Blixrow <75791514+Blixrow@users.noreply.github.com>
2023-11-27 16:41:51 +0100

    Test

--- src/routes/PickPage.tsx
+++ src/routes/PickPage.tsx
@@ -8,20 +8,7 @@ import { Link } from 'react-router-dom';
 export const Pick = () => {
   return <Container>
     <Grid2 container direction="row" justifyContent="center" alignItems="center">
-      <Grid2>
-        <Typography variant="h2" sx={{ mt: '140px', mb : '60px', fontFamily: 'Dancing Script', justifyContent : 'center' }}>
-          This is a test page !
-        </Typography>
-      </Grid2>
-      <Grid2 item xs={12}>
-        <Grid2 container justifyContent="center" alignItems="center" spacing={4}>
-
-        </Grid2>
-      </Grid2>
       <Grid2 container direction="row" justifyContent="center" alignItems="center" sx={{ mt : '12px', mb: '18px' }}>
-        <Grid2>
-          <Typography variant="h5" sx={{ fontFamily : 'Dancing Script' }}>Go to the next page :</Typography>
-        </Grid2>
         <Grid2>
           <Button component={Link} to="add" sx={{ ml: '18px', mt : '4px' }}>Click Here</Button>
         </Grid2>
