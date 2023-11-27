4e878899b4e7c6b7be6dcaa51579a5c7d258add8
blixrow <jbt@gmx.fr>
2023-11-27 18:00:53 +0100

    Prepa

--- src/routes/PickPage.tsx
+++ src/routes/PickPage.tsx
@@ -1,18 +1,20 @@
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-//import Grid2 from '@mui/material/Grid2';
 import Grid2 from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 
 export const Pick = () => {
   return <Container>
-    <Grid2 container direction="row" justifyContent="center" alignItems="center">
-      <Grid2 container direction="row" justifyContent="center" alignItems="center" sx={{ mt : '12px', mb: '18px' }}>
-        <Grid2>
-          <Button component={Link} to="add" sx={{ ml: '18px', mt : '4px' }}>Click Here</Button>
+      <Grid2 container direction="row" justifyContent="center" alignItems="center" sx={{ mt : '140px', mb: '18px' }}>
+        <Grid2 container direction="row" justifyContent="center" alignItems="center">
+          <Typography variant="h5" sx={{ fontFamily : 'Dancing Script' }}>Test 1 :</Typography>
+          <Button component={Link} to="add" sx={{ mt : '7px'}}>Click Here</Button>
+        </Grid2>
+        <Grid2 container direction="row" justifyContent="center" alignItems="center">
+          <Typography variant="h5" sx={{ fontFamily : 'Dancing Script' }}>Test 2 :</Typography>
+          <Button component={Link} to="add" sx={{ mt : '7px'}}>Click Here</Button>
         </Grid2>
       </Grid2>
-    </Grid2>
   </Container>
 }
