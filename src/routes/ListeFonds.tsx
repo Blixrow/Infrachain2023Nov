de0068669ab2afac9d082c49c00f411385c8c0e1
GIET Julien <julien.giet@telecomnancy.eu>
2023-11-28 12:44:43 +0100

    added some status boxes

--- src/routes/ListeFonds.tsx
+++ src/routes/ListeFonds.tsx
@@ -8,6 +8,8 @@ import Typography from '@mui/material/Typography';
 import { createBrowserHistory } from 'history';
 //import DetailsPage from './DetailsPage';
 import { useNavigate } from 'react-router-dom';
+import '../assets/ListeFonds.css'
+import { TextField, IconButton } from '@mui/material';
 
 const items: [string, string, string, string, string][] = [
   ["Item 1a", "1", "Statut 1", "NFT 1", "2023-11-27"],
@@ -28,16 +30,34 @@ const ListeFonds: React.FC = (): JSX.Element => {
     navigate("../details/"+1)
   };
 
+  function handleDeleteAction(index: number) {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <Container>
+      <div className="status-boxes">
+        <div className="box green-box">Validated</div>
+        <div className="box orange-box">Reported</div>
+        <div className="box red-box">To Do</div>
+      </div>
+
       <Grid container direction="column" justifyContent="center" alignItems="center">
+        {/* Barre de recherche */}
+        <Grid item xs={12} sx={{ mt: '20px' }}>
+          <TextField label="Recherche" variant="outlined" fullWidth />
+        </Grid>
+
+        {/* Titre de la liste */}
         <Grid item xs={12}>
-          <Typography variant="h2" sx={{ mt: '140px', mb: '60px', fontFamily: 'Dancing Script', justifyContent: 'center' }}>
+          <Typography variant="h2" sx={{ mt: '20px', mb: '60px', fontFamily: 'Dancing Script', justifyContent: 'center' }}>
             Liste des fonds
           </Typography>
         </Grid>
+
+        {/* Tableau */}
         <Grid item xs={12}>
-          <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '20px' }}>
+          <table style={{ width: '1000px', borderCollapse: 'collapse', marginTop: '20px' }}>
             <thead>
               <tr>
                 <th style={{ border: '1px solid black', padding: '8px' }}>Nom</th>
@@ -45,6 +65,7 @@ const ListeFonds: React.FC = (): JSX.Element => {
                 <th style={{ border: '1px solid black', padding: '8px' }}>Statut</th>
                 <th style={{ border: '1px solid black', padding: '8px' }}>NFT</th>
                 <th style={{ border: '1px solid black', padding: '8px' }}>Date</th>
+                <th style={{ border: '1px solid black', padding: '8px' }}>Actions</th>
               </tr>
             </thead>
             <tbody>
@@ -59,11 +80,19 @@ const ListeFonds: React.FC = (): JSX.Element => {
                       {item}
                     </td>
                   ))}
+                  <td>
+                    {/* Actions */}
+                    <IconButton onClick={() => handleDeleteAction(index)}>
+                      DeleteIcon ViewIcon EditIcon
+                    </IconButton>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
         </Grid>
+
+        {/* Bouton Ajouter un fond */}
         <Grid container direction="row" justifyContent="center" alignItems="center" sx={{ mt: '12px', mb: '18px' }}>
           <Grid item>
             <Typography variant="h5" sx={{ fontFamily: 'Dancing Script' }}>
@@ -76,6 +105,8 @@ const ListeFonds: React.FC = (): JSX.Element => {
             </Button>
           </Grid>
         </Grid>
+
+        {/* Bouton Voir les données publiques */}
         <Grid container direction="row" justifyContent="center" alignItems="center" sx={{ mt: '12px', mb: '18px' }}>
           <Grid item>
             <Typography variant="h5" sx={{ fontFamily: 'Dancing Script' }}>
