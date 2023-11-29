0a2a967085906e4cc8e61ebd006ae7702aa39843
GIET Julien <julien.giet@telecomnancy.eu>
2023-11-29 13:51:12 +0100

    small modifs

--- src/routes/PickPage.tsx
+++ src/routes/PickPage.tsx
@@ -4,13 +4,14 @@ import Container from '@mui/material/Container';
 import Grid2 from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
+import '../assets/PickPage.css'
 
 export const Pick = () => {
   return <Container>
     <Grid2 container direction="row" justifyContent="center" alignItems="center">
       <Grid2>
         <Typography variant="h2" sx={{ mt: '140px', mb : '60px', fontFamily: 'Dancing Script', justifyContent : 'center' }}>
-          This is a test page !
+          Welcome !
         </Typography>
       </Grid2>
       <Grid2 item xs={12}>
@@ -20,10 +21,10 @@ export const Pick = () => {
       </Grid2>
       <Grid2 container direction="row" justifyContent="center" alignItems="center" sx={{ mt : '12px', mb: '18px' }}>
         <Grid2>
-          <Typography variant="h5" sx={{ fontFamily : 'Dancing Script' }}>Go to the next page :</Typography>
+          <Typography variant="h5" sx={{ fontFamily : 'Dancing Script' }}></Typography>
         </Grid2>
         <Grid2>
-          <Button component={Link} to="../liste" sx={{ ml: '18px', mt : '4px' }}>Click Here</Button>
+          <Button className='page1-button' component={Link} to="../liste" sx={{ ml: '18px', mt : '4px', color: 'inherit', textDecoration: 'none' , padding: '1000px'}}></Button>
         </Grid2>
       </Grid2>
     </Grid2>
